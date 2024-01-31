f627907079953d309a5a8f866c5a2da6f58ff850
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-01-31 17:09:24 +0530

    portfolio update

---
+++ my-project/src/assets/index.js
@@ -0,0 +1,51 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nextjs from "./tech/next.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import bootstrap from "./tech/bootstrap.png";
+import hero from "./hero.png";
+
+import Saeculum from "./company/Saeculum.png";
+
+
+import carrent from "./carrent.png";
+
+
+export {
+  logo,
+  backend,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nextjs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  Saeculum,
+  carrent,
+  bootstrap,
+  hero
+};
