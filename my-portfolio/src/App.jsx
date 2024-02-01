db6e20e2b245ea2f5d7720d3ecbd6223dcf3021e
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-02-01 10:45:48 +0530

    work component update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {About, Experience, Hero, Navbar, Tech} from "./components";
+import {About, Experience, Hero, Navbar, Tech, Works} from "./components";
 
 const App = () => {
     return (
@@ -14,7 +14,7 @@ const App = () => {
                     <About />
                     <Experience />
                     <Tech />
-                    {/* <Works /> */}
+                    <Works />
                     <div className='relative z-0'>
                         {/* <Contact /> */}
                         {/* <StarsCanvas /> */}
--- my-portfolio/src/components/index.js
+++ my-portfolio/src/components/index.js
@@ -4,7 +4,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Experience from "./Experience";
 import Tech from "./Tech";
-// import Works from "./Works";
+import Works from "./Works";
 // import Contact from "./Contact";
 // import CanvasLoader from "./Loader";
 
@@ -15,7 +15,7 @@ export {
   About,
   Experience,
   Tech,
-  // Works,
+  Works,
   // Contact,
   // CanvasLoader,
   // EarthCanvas, 
