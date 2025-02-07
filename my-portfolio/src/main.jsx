b7fa6f3e58b8d850ebe99a6d40358df4eb1a1fd9
code-by-Prakhar <151377167+PrakharATripathi@users.noreply.github.com>
2025-02-07 22:40:08 +0530

    Update main.jsx

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SplashCursor from './components/CursorAnimation.jsx'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <SplashCursor/>
     <App />
   </>,
 )
